5c79fa82efccd9b0dccb36fbcd215a24bc3d8bf1
DanielCode37 <buechner.daniel@gmx.net>
2021-07-05 13:18:26 +0200

    added cards

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
-    res.send("HELLO WORLD")
+    res.sendFile("/index.html");
 });
 
-app.listen(process.env.PORT || 5000, console.log("Server started"));
+app.listen(process.env.PORT || 3000, () => console.log("Server started"));
