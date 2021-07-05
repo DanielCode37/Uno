14c7cd335a2071f70dc71bcef368d5c4680e8bd1
DanielCode37 <buechner.daniel@gmx.net>
2021-07-05 13:52:14 +0200

    added system for free cards

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const freeCards = require("./freeCards.json");
+
 app.use(express.static('public'));
 
 // app.use((req, res, next) => {
